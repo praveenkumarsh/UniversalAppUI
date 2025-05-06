705df8d41862b0c1a08a4b182448289702c40bc8
Praveen Sharma <dce.pks@gmail.com>
2025-05-07 04:26:07 +0530

    Updated config

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,6 @@
 export const config = {
   backendUrl: import.meta.env.VITE_BACKEND_URL,
   googleClientId: import.meta.env.VITE_GOOGLE_CLIENT_ID,
-  googleRedirectUri: import.meta.env.VITE_GOOGLE_REDIRECT_URI,
-  googleAuthUrl: `https://accounts.google.com/o/oauth2/v2/auth?client_id=${import.meta.env.VITE_GOOGLE_CLIENT_ID}&redirect_uri=${import.meta.env.VITE_GOOGLE_REDIRECT_URI}&response_type=code&scope=openid%20email%20profile&access_type=offline&prompt=consent`,
+  googleRedirectUri: `${import.meta.env.VITE_BACKEND_URL}${import.meta.env.VITE_GOOGLE_REDIRECT_URI}`,
+  googleAuthUrl: `https://accounts.google.com/o/oauth2/v2/auth?client_id=${import.meta.env.VITE_GOOGLE_CLIENT_ID}&redirect_uri=${import.meta.env.VITE_BACKEND_URL}${import.meta.env.VITE_GOOGLE_REDIRECT_URI}&response_type=code&scope=openid%20email%20profile&access_type=offline&prompt=consent`,
 };
